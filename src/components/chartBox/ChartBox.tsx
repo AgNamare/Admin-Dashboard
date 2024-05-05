47ccf383921f9ff08b7f3a9e530def618514b744
Augustine Namare <agNamare@gmail.com>
2024-05-05 15:41:03 +0300

    Bug fix

--- src/components/chartBox/ChartBox.tsx
+++ src/components/chartBox/ChartBox.tsx
@@ -1,55 +1,62 @@
-import React from 'react';
-import "./chartBox.scss"
-import {Link} from "react-router-dom"
-import {Tooltip, LineChart, Line, ResponsiveContainer } from 'recharts';
+import React from "react";
+import "./chartBox.scss";
+import { Link } from "react-router-dom";
+import { Tooltip, LineChart, Line, ResponsiveContainer } from "recharts";
 
 type Props = {
-    color: String,
-    icon: String,
-    title: string,
-    datakey: string,
-    number: number | string,
-    percentage: number,
-    charData: object[]
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
 
-}
-
-const ChartBox = (props:Prop) =>{
-    return(
-        <div className="chartBox">
-            <div className="boxInfo">
-                <div className="title">
-                    <img src={props.icon} alt="" />
-                    <span>{props.title}</span>
-                </div>
-                <h1>{props.number}</h1>
-                <Link to="/" style={{color:props.color}}>View all</Link>
-            </div>
-            <div className="chartInfo">
-                <div className="chart">
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart data={props.chartData}>
-                        <Tooltip 
-                        contentStyle={{background:"transparent", border:"none"}}
-                        labelStyle={{display:"none"}}
-                        position={{x:10, y:60}}
-                        />
-                        <Line 
-                            type="monotone"
-                            dataKey={props.dataKey} 
-                            stroke={props.color} 
-                            strokeWidth={2}
-                            dot={false}/>
-                    </LineChart>
-                </ResponsiveContainer>
-                </div>
-                <div className="texts">
-                    <span className="percentage" style={{color: props.percentage<0?"tomato": "limegreen"}}>{props.percentage}%</span>
-                    <span className="duration">this month</span>
-                </div>
-            </div>
+const ChartBox = (props: Props) => {
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src={props.icon} alt="" />
+          <span>{props.title}</span>
         </div>
-    )
-}
+        <h1>{props.number}</h1>
+        <Link to="/" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 60 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ChartBox
+export default ChartBox;
